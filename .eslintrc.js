a15a9def3a28016fce62390eacd291e04d8e8261
teerryn <slaughterer90@gmail.com>
2018-06-12 11:57:29 +0100

    Added favicon

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
 module.exports = {
-  extends: 'react-tools',
+  "root": true,
+  "extends": ["airbnb", "prettier", "prettier/react"],
+  "plugins": ["react", "prettier"],
+  "parserOptions": {
+    "ecmaVersion": 2016,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "classes": true,
+      "jsx": true
+    }
+  },
+  "env": {
+    "es6": true,
+    "browser": true,
+    "node": true,
+    "jest": true
+  },
+  "parser": "babel-eslint",
+  "rules": {
+    "linebreak-style": 0,
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        "components": ["Link"],
+        "specialLink": ["to"]
+      }
+    ],
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+  }
 }
--- static.config.js
+++ static.config.js
@@ -1,5 +1,7 @@
 import {Facebook} from 'fb';
 import React, { Component } from 'react';
+import ReactStaticFavicons from '@kuroku/react-static-favicons';
+var path = require('path');
 
 const getBlogData = async () => {
   const FB = new Facebook();
@@ -9,10 +11,27 @@ const getBlogData = async () => {
     grant_type: 'client_credentials',
   }); */
   
-  FB.setAccessToken("EAACEdEose0cBAIEQ3yUqEbRipwVJZCoZAvg6vXCcyb6Mj7ZB0rsgsn58egxfPBuc8KqA1hQC9VQRNNgigbNTK1Mg4W5xSfydSg1ydLjPPur4M99u4TTRyofj5ZCoJwRxxx6WX3RvGrQSWTYx6wRVdUNCas21KA5YcUAWZCwRURTyuuqYRLsjolyK2Ex8SoMMZD");
+  FB.setAccessToken("EAACEdEose0cBAN0gLZBuUIiZBJg0ZB6espMEoZARV8ajsXK1XXeqqBxYhyV3ZBv17hN2xGU9JzwaZBprvKksBzHTPoGzU1XsTZCUnqPnjuB74RR1VZBNTe9HyvaQCvfojBKcoPDgmVDlY6yXbwdZCWzfWe5G1QrXFkc8JUFqATRnQNK3hKsJCZBYFZBhZAJBZC0T7cQYZD");
   const { data } = await FB.api('/625206280892267/feed', { fields: 'story, message, created_time, full_picture, permalink_url' })
   return data;
 }
+
+const reactStaticFavicons = new ReactStaticFavicons({
+	// string: directory where the image files are written
+	outputDir: path.join(__dirname, 'dist'),
+
+	// string: the source image
+	inputFile: path.join(__dirname, 'src/logo.png'),
+
+	// object: the configuration passed directory to favicons
+	configuration: {
+		icons: {
+	        favicons: true,
+	        // other favicons configuration
+		}
+	},
+});
+
 
 
 export default {
@@ -51,6 +70,11 @@ export default {
       },
     ]
   },
+  renderToHtml: async (render, C, meta) => {
+		meta.faviconsElements = await reactStaticFavicons.render();
+		const html = render(<C />);
+		return html;
+	},
   Document: class CustomHtml extends Component {
     render () {
       const {
@@ -64,7 +88,7 @@ export default {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             {/* <link href="https://fonts.googleapis.com/css?family=Pacifico|Roboto:400,700" rel="stylesheet" /> */}
             <link href="https://fonts.googleapis.com/css?family=Kanit:300,700|Pacifico" rel="stylesheet" />
-            {renderMeta.styleTags}
+            {renderMeta.faviconsElements}
             <title>Bebés de Encantar</title>
           </Head>
           <Body>{children}</Body>
