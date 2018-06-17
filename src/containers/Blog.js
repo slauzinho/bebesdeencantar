90af8067cece2b2064c74bdee603cb047630e461
teerryn <slaughterer90@gmail.com>
2018-06-17 18:40:33 +0100

    Fixed loading state for blog posts

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { withRouteData } from "react-static";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -8,6 +7,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
 import Grid from '@material-ui/core/Grid';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const CardMediaStyle = styled(CardMedia)`
   height: 15rem;
@@ -30,9 +30,18 @@ const CardActionStyle = styled(CardActions)`
   align-self: flex-end;
 `
 
+const LoadingStyle = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const CircleStyled = styled(CircularProgress)`
+  color: #ff6f96 !important;
+`
+
 export default class Blog extends React.Component {
   state = {
-    posts: []
+    posts: null
   }
   async componentDidMount() {
     fetch(`${process.env.LAMBDA_ENDPOINT}posts`).then(res => {
@@ -43,62 +52,34 @@ export default class Blog extends React.Component {
   }
   render() {
     const { posts } = this.state;
-    if (posts === []) {
-      return <div>loading</div>
+    
+    if (posts) {
+      return (
+        <Grid container spacing={0} alignItems="stretch">
+      {posts.map(post => (
+        <GridItemStyle item xs={12} sm={6} key={posts["created_time"]}>
+        <CardStyle>
+        <div>
+  
+          <CardMediaStyle
+            image={post["full_picture"]}
+            title={post.story}
+          />
+          <CardContent>
+            <Typography component="p">{post.message}</Typography>
+          </CardContent>
+        </div>
+          <CardActionStyle>
+            <Button size="small" color="primary" href={post['permalink_url']}>
+              Consultar
+            </Button>
+          </CardActionStyle> 
+        </CardStyle>
+        </GridItemStyle>
+      ))}
+    </Grid>
+      )
     }
-
-    return (
-      <Grid container spacing={0} alignItems="stretch">
-    {posts.map(post => (
-      <GridItemStyle item xs={12} sm={6} key={posts["created_time"]}>
-      <CardStyle>
-      <div>
-
-        <CardMediaStyle
-          image={post["full_picture"]}
-          title={post.story}
-        />
-        <CardContent>
-          <Typography component="p">{post.message}</Typography>
-        </CardContent>
-      </div>
-        <CardActionStyle>
-          <Button size="small" color="primary" href={post['permalink_url']}>
-            Consultar
-          </Button>
-        </CardActionStyle> 
-      </CardStyle>
-      </GridItemStyle>
-    ))}
-  </Grid>
-    )
+   return <LoadingStyle><CircleStyled size={50} /></LoadingStyle>
   }
 }
-
-
-/* export default withRouteData(({ posts }) => (
-  <Grid container spacing={0} alignItems="stretch">
-    {posts.map(post => (
-      <GridItemStyle item xs={12} sm={6} key={posts["created_time"]}>
-      <CardStyle>
-      <div>
-
-        <CardMediaStyle
-          image={post["full_picture"]}
-          title={post.story}
-        />
-        <CardContent>
-          <Typography component="p">{post.message}</Typography>
-        </CardContent>
-      </div>
-        <CardActionStyle>
-          <Button size="small" color="primary" href={post['permalink_url']}>
-            Consultar
-          </Button>
-        </CardActionStyle> 
-      </CardStyle>
-      </GridItemStyle>
-    ))}
-  </Grid>
-
-)); */
